23680a54ae68defec6063b541e4974b312db4d59
LEBaz2211 <92997786+LEBaz2211@users.noreply.github.com>
2022-12-12 17:19:55 +0100

    changed port to 80

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+app.listen(80, () => {
+    console.log('Server is running on port 80')
 })
