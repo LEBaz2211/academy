745ff8c96308e14ab3afc66dcb95b96c95c79287
LEBaz2211 <92997786+LEBaz2211@users.noreply.github.com>
2022-12-14 16:23:09 +0100

    database

--- dataBase.js
+++ dataBase.js
@@ -4,7 +4,7 @@ let mysql = require("mysql")
 let connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: 'root',
+    password: 'MemyselfandI269',
     database: 'lectures'
 })
 
